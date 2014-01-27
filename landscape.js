cbf972992c4dd27c84d27dfc2e6a8db728ca1b55
Robin Berjon <robin@berjon.com>
2014-01-27 11:23:18 +0100

    import the stuff

---
+++ landscape.js
@@ -0,0 +1,60 @@
+
+(function ($) {
+    var data = {}
+    ,   typeKey = {
+            ua:     "user agent"
+        ,   val:    "validity"
+        ,   auth:   "authoring advice"
+        ,   op:     "opinion"
+        }
+    ,   rationaleKey = {
+            impl:   "Insufficient implementation experience at this point."
+        ,   a11y:   "People in the accessibility community feel this alternative is better."
+        }
+    ;
+    function loadData (cb) {
+        var sources = "what-51 51-50".split(" ")
+        ,   done = 0;
+        $.each(sources, function (idx, src) {
+            $.getJSON(src + ".json", function (json) {
+                data[src] = json;
+                done++;
+                if (done === sources.length) cb();
+            });
+        });
+    }
+    function render (id) {
+        var $target = $("#" + id);
+        $.each(data[id], function (idx, item) {
+            var rat = /\s/.test(item.rationale) ? item.rationale : rationaleKey[item.rationale]
+            , $div = $("<div class=diff><h3></h3><p></p></div>")
+                        .find("h3")
+                            .text(item.title + " ")
+                            .append($("<span class=type></span>").text("(" + typeKey[item.type] + ")"))
+                        .end()
+                        .find("p")
+                            .html(rat)
+                        .end()
+                        .appendTo($target)
+            ;
+            if (item.link) {
+                var text = typeof item.link === "string" ? item.link : item.link.text
+                ,   url  = typeof item.link === "string" ? item.link : item.link.url
+                ;
+                $("<p class=more><a></a></p>")
+                    .find("a")
+                        .attr("href", url)
+                        .text(text)
+                    .end()
+                    .appendTo($div)
+                ;
+            }
+        });
+    }
+    $(function () { loadData(function () {
+        data["what-50"] = data["what-51"].concat(data["51-50"]);
+        render("what-51");
+        render("51-50");
+        render("what-50");
+    }); });
+}(jQuery));
